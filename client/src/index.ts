d7660a4c649f29bb2125bc716478614829641245
Lucas Shadler <lxs2208@rit.edu>
2020-06-28 20:19:51 -0700

    fix: create scripts structure

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,10 @@
 import * as puppeteer from 'puppeteer';
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        product: 'chrome',
+        headless: false,
+    });
     const page = await browser.newPage();
     await page.goto('https://google.com');
     await page.pdf({ path: 'google.pdf' });
