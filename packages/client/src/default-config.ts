00ca0efb066a0f6a0426fd71e516f72b1597837d
lshadler <lshadler13@gmail.com>
2020-06-28 22:26:00 -0700

    feat: support firefox auto-install

---
+++ packages/client/src/default-config.ts
@@ -0,0 +1,18 @@
+import { MulticamOptions } from './types';
+
+export const baseConfig: MulticamOptions = {
+    'all-favorites': [
+        '59aedf63b5e8310014bbe375:',
+        '5842041f4e65fad6a770883f:',
+        '5842041f4e65fad6a7708842:',
+        '5842041f4e65fad6a7708841:',
+        '5842041f4e65fad6a77088c4:',
+        '5842041f4e65fad6a77088cc:',
+        '5842041f4e65fad6a7708839:',
+        '5842041f4e65fad6a77088af:',
+        '5842041f4e65fad6a77088b4:',
+    ],
+    numCams: { 'all-favorites': 9 },
+};
+
+export default baseConfig;
--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,13 +1,39 @@
 import * as puppeteer from 'puppeteer';
+import { getCredentials } from './credentials';
+import { Credentials } from './types';
+import { baseConfig } from './default-config';
 
 (async () => {
     const browser = await puppeteer.launch({
-        product: 'chrome',
+        product: 'firefox',
         headless: false,
-        args: ['--no-sandbox'],
+        args: ['--no-sandbox', '--start-maximized', '--start-fullscreen'],
     });
+
     const page = await browser.newPage();
-    await page.goto('https://google.com');
-    await page.pdf({ path: 'google.pdf' });
-    await browser.close();
+    await page.setViewport({ width: 1366, height: 768 });
+
+    const surflineCredentials: Credentials = getCredentials();
+
+    await page.goto('https://www.surfline.com/sign-in?');
+
+    await page.waitFor('#email');
+
+    await page.type('#email', surflineCredentials.username, { delay: 20 });
+    await page.type('#password', surflineCredentials.password, { delay: 20 });
+
+    await page.click('form button.quiver-button');
+
+    await page.waitFor('a.quiver-navigation-item__container__link');
+
+    // add correct favorites
+    await page.evaluate((config) => {
+        localStorage.setItem('multi-cam:preferences', config);
+    }, JSON.stringify(baseConfig));
+
+    await page.goto('https://www.surfline.com/surf-cams?');
+
+    // Enter fullscreen
+    await page.waitFor('div.sl-multi-cam-container');
+    await page.click('div.sl-multi-cam__controls button.sl-fullscreen-toggle-button');
 })();
